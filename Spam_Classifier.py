1c6082ad9352d48fb90c3c5cf50b917ae2386936
vivek bhoj <vivekbhoj2208@gmail.com>
2020-07-14 16:18:45 +0530

    Add files via upload

---
+++ Spam_Classifier.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 12 11:37:26 2020
+
+@author: vbhoj
+"""
+
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import pickle
+
+messages = pd.read_csv(r"C:\Users\vbhoj\Downloads\NLP\Spam Classfier\smsspamcollection\SMSSpamCollection",
+                 sep="\t" ,names=['label','message'])
+
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+# stemming
+corpus = []
+
+for i in range(0,len(messages)):
+    review = re.sub('[^a-zA-Z]', ' ',messages['message'][i])
+    # print(review)
+    
+    review  =review.lower()
+    review = review.split()
+    # print(review)
+    review = [stemmer.stem(word) for word in review if word not in stopwords.words("english")]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+    
+    
+# Bag of words    
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=2500)
+X = cv.fit_transform(corpus).toarray()
+
+y = pd.get_dummies(messages['label'])
+y = y.iloc[:,1]
+
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = .20 , random_state = 0)
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(X_train,y_train)
+
+
+# prediction of the spam
+y_pred = spam_detect_model.predict(X_test)
+
+
+from sklearn.metrics import confusion_matrix
+cf = confusion_matrix(y_test,y_pred)
+
+'Accuracy score'
+from sklearn.metrics import accuracy_score
+score = accuracy_score(y_test,y_pred)
+
+
+# saving model file into disk
+pickle.dump(spam_detect_model, open('Spam_Classifier.pkl','wb'))
+
+
+
---
+++ app.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 12 13:03:49 2020
+
+@author: vbhoj
+"""
+
+#pip install flask
+from flask import Flask, render_template, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('Spam_Classifier.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('spam.html')
+        # return "<center><h1>Welcome to homepage</h1></center>"
+
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    sentence = request.form['spamclass']
+    data = [sentence]
+    # print(data,type(data),sentence,type(sentence))
+    vect = cv.transform(data).toarray()    
+    output = spam_detect_model.predict(vect)
+    # print(output,"outputttttttttttttttttt")
+    if output is ():
+        return "There is some error"
+    else:
+        if output == 0:
+            return render_template('spam.html', prediction_text = 'This text does not have spam words')
+        else:
+            return render_template('spam.html', prediction_text = 'This text contains spam words')
+# # app.run()
+
+if __name__ == "__main__":
+    # app.run(debug = True)
+    app.run()
+       
+    
+    
